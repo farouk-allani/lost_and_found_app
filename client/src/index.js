40526ae8c6639603d90534f8937dfc562fbde6cb
Farouk Allani <farouk.allani.pro@gmail.com>
2022-03-02 16:24:11 +0100

    make it better

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,8 @@ import store from "./JS/store/store";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-
+import axios from "axios";
+axios.defaults.baseURL='https://lost-and-found-in-tunisia.herokuapp.com/';
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
